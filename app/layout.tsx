1b4fba388301c97c541b507aba587fecbd3ca3b9
Kabir Asani <iamkabirasani@gmail.com>
2025-04-16 10:38:31 +0530

    Update dependencies in package.json
    - Added @radix-ui/themes and next-themes to dependencies
    - Updated next to version 15.3.0
    - Updated @eslint/eslintrc in devDependencies
    - Rearranged dependencies for better organization

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,9 @@
+import { Theme } from "@radix-ui/themes";
+import "./globals.css";
+
 import { Metadata } from "next";
 import { PropsWithChildren } from "react";
+import { ThemeProvider } from "@/components/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "Nickel",
@@ -8,8 +12,12 @@ export const metadata: Metadata = {
 
 const RootLayout = (props: PropsWithChildren) => {
   return (
-    <html>
-      <body>{props.children}</body>
+    <html suppressHydrationWarning>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          <Theme accentColor="indigo">{props.children}</Theme>
+        </ThemeProvider>
+      </body>
     </html>
   );
 };
