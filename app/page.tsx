6fad79820397a3384542d97459af34b41a7bdb74
Kabir Asani <iamkabirasani@gmail.com>
2025-04-16 11:13:02 +0530

    Add HeroBlock component and update RootPage to use it

--- app/page.tsx
+++ app/page.tsx
@@ -1,42 +1,9 @@
 import { gray } from "@radix-ui/colors";
-import { Box, Button, Container, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Container, Flex, Heading, Separator, Text } from "@radix-ui/themes";
+import HeroBlock from "./blocks/HeroBlock";
 
 const RootPage = () => {
-  return (
-    <Box>
-      <Container>
-        <Box
-          style={{
-            border: "1px",
-            borderStyle: "dotted",
-            borderColor: gray.gray6,
-          }}
-        >
-          <Flex direction="column" align="center" gap="8">
-            <Heading align="center" size="9">
-              The React Framework for the Web
-            </Heading>
-            <Text
-              align="center"
-              style={{
-                maxWidth: "776px",
-              }}
-            >
-              Used by some of the world's largest companies, Next.js enables you to create high-quality web applications
-              with the power of React components.
-            </Text>
-
-            <Flex direction="row" gap="4">
-              <Button size="4">Get Started</Button>
-              <Button variant="soft" size="4">
-                Learn Next.js
-              </Button>
-            </Flex>
-          </Flex>
-        </Box>
-      </Container>
-    </Box>
-  );
+  return <HeroBlock />;
 };
 
 export default RootPage;
